b6aa4743e3bb7370143c161cf6e5b5eebac245d3
klimoza <i220303@mail.ru>
2021-02-03 17:54:23 +0300

    Change font to JetBrains Mono, add flameshot configuration

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,6 +8,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar main -r -c "$HOME/.config/polybar/config" &
+polybar klimoza -r -c "$HOME/.config/polybar/config" &
 
 echo "Polybar launched..."
